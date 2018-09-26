0fb1c5d0a2f087845874e183216ec8968d3d8104
guysv <sviryguy@gmail.com>
2018-09-26 16:30:38 +0300

    Relicense as GPLv2

--- txkernel/__init__.py
+++ txkernel/__init__.py
@@ -1,6 +1,6 @@
 # txkernel
 # Copyright (C) 2018  guysv
 
-# This file is part of txkernel which is released under GPLv3.
-# See file LICENSE or go to https://www.gnu.org/licenses/gpl-3.0.txt
+# This file is part of txkernel which is released under GPLv2.
+# See file LICENSE or go to https://www.gnu.org/licenses/gpl-2.0.txt
 # for full license details.
